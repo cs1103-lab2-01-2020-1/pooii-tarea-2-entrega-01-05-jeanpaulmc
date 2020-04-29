7cb6e4dbef03dce7b21fb43fe64e9e2c8275298a
jeanpaulmc <44785966+jeanpaulmc@users.noreply.github.com>
2020-04-29 17:38:59 -0500

    Resuelto el problema 3

--- src/ejercicios.cpp
+++ src/ejercicios.cpp
@@ -13,12 +13,24 @@ void ejercicio2(){
 }
 
 void ejercicio3(){
+    cout << "Vector" << endl;
     int arr1[] = {1, 2, 3, 4, 5};
     int n1 = 5;
     int arr2[] = {6, 7, 8, 9, 10};
     int n2 = 5;
     print_arr(arr1, n1);
     print_arr(arr2, n2);
+    cout << "Lista" << endl;
+    list<int> l1(begin(arr1), end(arr1));
+    for(auto x:l1){
+        cout << x << " ";
+    }
+    cout << endl;
+    list<int> l2(begin(arr2), end(arr2));
+    for(auto x:l2){
+        cout << x << " ";
+    }
+    cout << endl;
 }
 
 void ejercicio4(){
